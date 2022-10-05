352c5785c18682cc5da847c87109ec0a98804739
Muhammad Bilal <muhammadbilal@nxb.com.pk>
2022-10-05 16:34:10 +0500

    Change imports astructire and import from package called common-lib-myproject

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,23 +1,21 @@
-import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
-import jwt from 'jsonwebtoken';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
 
-import { validateRequest } from '../middlewares/validate-request';
-import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from "common-lib-myproject";
+import { User } from "../models/user";
+import { BadRequestError } from "common-lib-myproject";
 
 const router = express.Router();
 
 router.post(
-  '/api/users/signup',
+  "/api/users/signup",
   [
-    body('email')
-      .isEmail()
-      .withMessage('Email must be valid'),
-    body('password')
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage('Password must be between 4 and 20 characters')
+      .withMessage("Password must be between 4 and 20 characters"),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
@@ -26,7 +24,7 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new BadRequestError('Email in use');
+      throw new BadRequestError("Email in use");
     }
 
     const user = User.build({ email, password });
@@ -36,14 +34,14 @@ router.post(
     const userJwt = jwt.sign(
       {
         id: user.id,
-        email: user.email
+        email: user.email,
       },
       process.env.JWT_KEY!
     );
 
     // Store it on session object
     req.session = {
-      jwt: userJwt
+      jwt: userJwt,
     };
 
     res.status(201).send(user);
