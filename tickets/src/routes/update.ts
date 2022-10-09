50a940a9a7597e2f15f5a6ba8e19af3ec6e2cf1d
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-10-09 23:02:58 +0500

    Create mock test for nats-wrapper with mock directory and on ADD UPDATE events called events as well of NATS

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,23 +1,25 @@
-import express, { Request, Response } from "express";
-import { body } from "express-validator";
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 import {
   validateRequest,
   NotFoundError,
   requireAuth,
   NotAuthorizedError,
-} from "common-lib-myproject";
-import { Ticket } from "../models/ticket";
+} from '@cygnetops/common';
+import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.put(
-  "/api/tickets/:id",
+  '/api/tickets/:id',
   requireAuth,
   [
-    body("title").not().isEmpty().withMessage("Title is required"),
-    body("price")
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
       .isFloat({ gt: 0 })
-      .withMessage("Price must be provided and must be greater than 0"),
+      .withMessage('Price must be provided and must be greater than 0'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
@@ -36,6 +38,12 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
