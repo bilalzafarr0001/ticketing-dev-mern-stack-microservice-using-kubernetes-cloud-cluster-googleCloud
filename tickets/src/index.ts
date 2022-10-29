a29a991e2210cef6f408affd0c92e74554c201ec
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-10-29 19:54:19 +0500

    Adding test message in all service files

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,6 +5,8 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
+  console.log('Tickets service is getting up ..')
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
