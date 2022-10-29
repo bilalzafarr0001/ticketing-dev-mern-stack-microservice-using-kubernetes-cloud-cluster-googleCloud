cc11e7af072b07ee35d289ee9e744f1471f365ff
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-10-29 20:04:51 +0500

    testing clientside-deployment

--- client/pages/index.js
+++ client/pages/index.js
@@ -17,7 +17,7 @@ const LandingPage = ({ currentUser, tickets }) => {
 
   return (
     <div>
-      <h2>Tickets</h2>
+      <h2>Ticketss</h2>
       <table className="table">
         <thead>
           <tr>
